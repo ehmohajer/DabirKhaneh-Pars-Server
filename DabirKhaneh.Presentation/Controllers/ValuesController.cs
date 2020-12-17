dd6eb8c4171a86d2c97f34742589b396cc88ad0d
ehsan <eh.mohajer@gmail.com>
2020-12-17 14:17:17 +0330

    init project

---
+++ DabirKhaneh.Presentation/Controllers/ValuesController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DabirKhaneh.Presentation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : Controller
+    {
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<string>>> Get()
+        {
+            return new string[] { "Value1", "Value2" };
+        }
+    }
+}
